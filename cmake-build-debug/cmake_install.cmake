528f387171e221a3a7146757c17e4d8687281f94
RasmusBastian <rbbjensen@gmail.com>
2024-11-22 11:51:51 +0100

    ga ya allah rasmus

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/jespersoerensen/CLionProjects/mine_sweeper_p1
+# Install script for directory: /Users/rasmusbastianbang/CLionProjects/mine_sweeper_p1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/jespersoerensen/CLionProjects/mine_sweeper_p1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/rasmusbastianbang/CLionProjects/mine_sweeper_p1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
