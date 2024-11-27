f3cd8fd0634b99256d030b74adc694c4a6dc4399
RasmusBastian <rbbjensen@gmail.com>
2024-11-27 10:06:02 +0100

    27/11 ændringer for structs

--- src/algorithm/run_algorithm.c
+++ src/algorithm/run_algorithm.c
@@ -3,26 +3,13 @@
 #include <math.h>
 #include <stdlib.h>
 
-typedef struct is_obstacle {
-    int is_tree;
-    int is_stone;
-    int is_bush;
-    int is_tripwire;
-    int is_deep_water;
-    int obstacle_cost;          // 1
-}is_obstacle;
 
-typedef struct is_mine {
-    int MRUD;                   // Værdi 1
-    int PMA2;                   // Værdi 2
-    int PMA3;                   // Værdi 3
-    int PROM1;                  // Værdi 4
-} is_mine;
 
-typedef struct elevation {
-    int height;         // Højden for en given celle
-    int movement_cost;  // Ekstra omkostning baseret på højde
-} Elevation;
+typedef struct map_index {
+    int is_obstacle;        // Obstacle type / Ingen obstacle == 0
+    int  is_mine;           // Minetype / Ingen mine == 0
+    int elevation;          // Højden for en given celle
+};
 
 typedef struct node {
     int x, y;              // Koordinater
@@ -30,9 +17,7 @@ typedef struct node {
     int h_cost;            // Omkostningen fra denne node til mål
     int f_cost;            // Summen af g_cost og h_cost (f_cost = g_cost + h_cost)
     struct node* parent;   // Pointer til forælder-node for sti-rekonstruktion
-    struct is_obstacle;    // 1 for forhindring 0 for passerbar
-    struct  is_mine;       // Vurdere om der er en mine (hvilken mine) eller ingen mine
-    struct elevation;      // Vurdere højdeforskel
+    struct map_index;      // Obstacle, landminer, højdeforskel
     int risk_level;        // Ekstra felt til at angive risiko-niveau (f.eks. miner: højere værdi)
 } node;
 
