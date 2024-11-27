48e9c81385532fb925a7bf0a5e43a1c056e4614f
Mae Chee Delicana <mdelic22@student.aau.dk>
2024-11-27 09:13:22 +0100

    Skrevet i forkerte branch (fikset)

--- src/map_processing/parse_map.c
+++ src/map_processing/parse_map.c
@@ -8,20 +8,6 @@
 // Output a 2D array representing the map.
 #include <stdio.h>
 
-//definerer coordinates variablen som indeholder x og y
-struct coordinates{int x; int y;};
-typedef struct coordinates coordinates;
-
-//her får vi vores data fra read_map og omdanner til vores coordinates(?)
-void input_data(char *get_read_map) {
-
-}
-//Make an array fitting the coordinates
-void map_split(int array[]);
-
-
-void output_data();
-
 void parse_map_test() {
     printf("parse_map works\n");
 
